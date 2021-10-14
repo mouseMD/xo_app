d65cf1c9e35d0f82a7a77a6de55c85b50c9b40cb
Denis Shihalev <shihalev.denis@gmail.com>
2021-10-14 21:53:42 +0300

    Restyle braces

--- src/board_4_4_4.cpp
+++ src/board_4_4_4.cpp
@@ -3,116 +3,110 @@
 #include <iostream>
 #include <string>
 
-#include "winchecker.h"
 #include "bitboardhelpers.h"
+#include "winchecker.h"
 
-Board_4_4_4::Board_4_4_4()
-{
-    clear();
-}
+Board_4_4_4::Board_4_4_4() { clear(); }
 
 void Board_4_4_4::clear()
 {
-    xs_ = 0;
-    os_ = 0;
-    win_ = 0;
+  xs_ = 0;
+  os_ = 0;
+  win_ = 0;
 }
 
 void Board_4_4_4::setMark(Player player, const Coordinates &coord)
 {
-    BITBOARD new_move = getBoardFromCoordinates(coord);
-    if (player == X_Player)
+  BITBOARD new_move = getBoardFromCoordinates(coord);
+  if (player == X_Player)
+  {
+    if (!(new_move & os_))
     {
-        if (!(new_move & os_))
-        {
-            xs_ |= new_move;
-        }
+      xs_ |= new_move;
     }
-    else if (player == O_Player)
+  }
+  else if (player == O_Player)
+  {
+    if (!(new_move & xs_))
     {
-        if (!(new_move & xs_))
-        {
-            os_ |= new_move;
-        }
+      os_ |= new_move;
     }
+  }
 }
 
 bool Board_4_4_4::checkWin(Player player) const
 {
-    if (player == X_Player)
-    {
-        return checkWin_4_4_4(xs_, win_);
-    }
-    else if (player == O_Player)
-    {
-        return checkWin_4_4_4(os_, win_);
-    }
-    else
-        return false;
+  if (player == X_Player)
+  {
+    return checkWin_4_4_4(xs_, win_);
+  }
+  else if (player == O_Player)
+  {
+    return checkWin_4_4_4(os_, win_);
+  }
+  else
+    return false;
 }
 
-bool Board_4_4_4::checkDraw() const
-{
-    return ~(xs_ | os_) == 0;
-}
+bool Board_4_4_4::checkDraw() const { return ~(xs_ | os_) == 0; }
 
 std::array<Coordinates, 4> Board_4_4_4::getWinCoords() const
 {
-    if (win_)
-        return getWinCoordsFromBoard(win_);
-    else
-        return {};
+  if (win_)
+    return getWinCoordsFromBoard(win_);
+  else
+    return {};
 }
 
 void Board_4_4_4::debugPrint()
 {
-    std::cout << "*" << std::endl;
-    for (uint8_t i = 0; i<4; ++i)
+  std::cout << "*" << std::endl;
+  for (uint8_t i = 0; i < 4; ++i)
+  {
+    std::string line;
+    for (uint8_t j = 0; j < 4; ++j)
     {
-        std::string line;
-        for (uint8_t j = 0; j<4; ++j)
+      for (uint8_t k = 0; k < 4; ++k)
+      {
+        BITBOARD bb = getBoardFromCoordinates(Coordinates(j, k, i));
+        if (bb & xs_)
         {
-            for (uint8_t k = 0; k<4; ++k)
-            {
-                BITBOARD bb = getBoardFromCoordinates(Coordinates(j, k, i));
-                if (bb & xs_)
-                {
-                    line += "X";
-                }
-                else if (bb & os_)
-                {
-                    line += "O";
-                }
-                else
-                {
-                    line += ".";
-                }
-            }
-            line += "    ";
+          line += "X";
         }
-        std::cout << line << std::endl;
+        else if (bb & os_)
+        {
+          line += "O";
+        }
+        else
+        {
+          line += ".";
+        }
+      }
+      line += "    ";
     }
+    std::cout << line << std::endl;
+  }
 }
 
 std::array<Player, 64> Board_4_4_4::getBoard() const
 {
-    std::array<Player, 64> result;
+  std::array<Player, 64> result;
 
-    for (std::size_t i = 0; i < 64; ++i)
+  for (std::size_t i = 0; i < 64; ++i)
+  {
+    BITBOARD mask = 1ull << i;
+    if (xs_ & mask)
     {
-        BITBOARD mask = 1ull << i;
-        if (xs_ & mask)
-        {
-            result[i] = Player::X_Player;
-        }
-        else if (os_ & mask)
-        {
-            result[i] = Player::O_Player;
-        }
-        else
-        {
-            result[i] = Player::None;
-        }
+      result[i] = Player::X_Player;
     }
-    return result;
+    else if (os_ & mask)
+    {
+      result[i] = Player::O_Player;
+    }
+    else
+    {
+      result[i] = Player::None;
+    }
+  }
+  return result;
 }
--- src/board_4_4_4.h
+++ src/board_4_4_4.h
@@ -3,24 +3,25 @@
 
 #include <array>
 
-#include "common_types.h"
 #include "board_types.h"
+#include "common_types.h"
 
 class Board_4_4_4
 {
-public:
-    Board_4_4_4();
-    void clear();
-    void setMark(Player player, const Coordinates& coord);
-    bool checkWin(Player player) const;
-    bool checkDraw() const;
-    std::array<Coordinates, 4> getWinCoords() const;
-    void debugPrint();
-    std::array<Player, 64> getBoard() const;
-private:
-    BITBOARD xs_;
-    BITBOARD os_;
-    mutable BITBOARD win_;
+ public:
+  Board_4_4_4();
+  void clear();
+  void setMark(Player player, const Coordinates& coord);
+  bool checkWin(Player player) const;
+  bool checkDraw() const;
+  std::array<Coordinates, 4> getWinCoords() const;
+  void debugPrint();
+  std::array<Player, 64> getBoard() const;
+
+ private:
+  BITBOARD xs_;
+  BITBOARD os_;
+  mutable BITBOARD win_;
 };
 
-#endif // BOARD_4_4_4_H
+#endif  // BOARD_4_4_4_H
