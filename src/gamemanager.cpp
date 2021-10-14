8f6d680fd53500aa238b24028b39bd9a6063b3ee
Denis Shihalev <shihalev.denis@gmail.com>
2021-10-14 21:46:16 +0300

    Restyle gamemanager

--- src/gamemanager.cpp
+++ src/gamemanager.cpp
@@ -1,28 +1,21 @@
 #include "gamemanager.h"
 
-GameManager::GameManager():currentMaxIndex_(0)
-{
-
-}
+GameManager::GameManager() : currentMaxIndex_(0) {}
 
 GAME_INDEX GameManager::createNewGame()
 {
-    currentMaxIndex_++; // Game indexes start from 1
-    games.emplace(currentMaxIndex_, currentMaxIndex_);  // implicit construction std::pair<GAME_INDEX, Game3D_4_4_4>
-    return currentMaxIndex_;
+  currentMaxIndex_++;  // Game indexes start from 1
+  games.emplace(currentMaxIndex_,
+                currentMaxIndex_);  // implicit construction
+                                    // std::pair<GAME_INDEX, Game3D_4_4_4>
+  return currentMaxIndex_;
 }
 
-void GameManager::deleteGame(GAME_INDEX index)
-{
-    games.erase(index);
-}
+void GameManager::deleteGame(GAME_INDEX index) { games.erase(index); }
 
-Game3D_4_4_4 &GameManager::getGame(GAME_INDEX index)
-{
-    return games.at(index);
-}
+Game3D_4_4_4 &GameManager::getGame(GAME_INDEX index) { return games.at(index); }
 
 const Game3D_4_4_4 &GameManager::getGame(GAME_INDEX index) const
 {
-    return games.at(index);
+  return games.at(index);
 }
--- src/gamemanager.h
+++ src/gamemanager.h
@@ -4,21 +4,22 @@
 #include <unordered_map>
 
 #include "common_types.h"
-#include "game_types.h"
 #include "game3d_4_4_4.h"
+#include "game_types.h"
 
 // Manages registry of games
 class GameManager
 {
-public:
-    GameManager();
-    GAME_INDEX createNewGame();
-    void deleteGame(GAME_INDEX index);
-    Game3D_4_4_4& getGame(GAME_INDEX index);
-    const Game3D_4_4_4& getGame(GAME_INDEX index) const;
-private:
-    std::unordered_map<GAME_INDEX, Game3D_4_4_4> games;
-    GAME_INDEX currentMaxIndex_;
+ public:
+  GameManager();
+  GAME_INDEX createNewGame();
+  void deleteGame(GAME_INDEX index);
+  Game3D_4_4_4& getGame(GAME_INDEX index);
+  const Game3D_4_4_4& getGame(GAME_INDEX index) const;
+
+ private:
+  std::unordered_map<GAME_INDEX, Game3D_4_4_4> games;
+  GAME_INDEX currentMaxIndex_;
 };
 
-#endif // GAMEMANAGER_H
+#endif  // GAMEMANAGER_H
