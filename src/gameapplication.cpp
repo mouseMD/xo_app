51b62e475faec39ca4ab731f91a17240254ec1d2
Denis Shihalev <shihalev.denis@gmail.com>
2021-10-14 21:49:39 +0300

    Restyle gameapplication

--- src/gameapplication.cpp
+++ src/gameapplication.cpp
@@ -1,105 +1,102 @@
 #include "gameapplication.h"
 
-#include <iostream>
-#include "magic.h"
 #include <algorithm>
+#include <iostream>
+
+#include "magic.h"
 
 GameApplication::GameApplication()
 {
-    // init global data
-    magic::generateWinPositions();
+  // init global data
+  magic::generateWinPositions();
 }
 
-void GameApplication::init()
-{
-
-}
+void GameApplication::init() {}
 
 void GameApplication::debugPrint(GAME_INDEX index)
 {
-    manager_.getGame(index).debugPrint();
+  manager_.getGame(index).debugPrint();
 }
 
-GAME_INDEX GameApplication::createNewGame()
-{
-    return manager_.createNewGame();
-}
+GAME_INDEX GameApplication::createNewGame() { return manager_.createNewGame(); }
 
-void GameApplication::setNewMove(GAME_INDEX index, Player player, const Coordinates &coords)
+void GameApplication::setNewMove(GAME_INDEX index, Player player,
+                                 const Coordinates& coords)
 {
-    manager_.getGame(index).setNewMove(player, coords);
+  manager_.getGame(index).setNewMove(player, coords);
 }
 
 void GameApplication::releaseGame(GAME_INDEX index)
 {
-    manager_.deleteGame(index);
+  manager_.deleteGame(index);
 }
 
-void GameApplication::finalize()
-{
-
-}
+void GameApplication::finalize() {}
 
 bool GameApplication::finished(GAME_INDEX index) const
 {
-    return manager_.getGame(index).isFinished();
+  return manager_.getGame(index).isFinished();
 }
 
 bool GameApplication::started(GAME_INDEX index) const
 {
-    return manager_.getGame(index).isInProcess();
+  return manager_.getGame(index).isInProcess();
 }
 
 uint8_t GameApplication::result(GAME_INDEX index) const
 {
-    return manager_.getGame(index).result();
+  return manager_.getGame(index).result();
 }
 
 std::string GameApplication::getMoves(GAME_INDEX index) const
 {
-    std::string line;
-    auto moves = manager_.getGame(index).getMoves();
-    for (auto move : moves)
-    {
-        line += std::to_string(move.square_);
-        line += std::to_string(move.vertical_);
-        line += std::to_string(move.horizontal_);
-    }
-    return line;
+  std::string line;
+  auto moves = manager_.getGame(index).getMoves();
+  for (auto move : moves)
+  {
+    line += std::to_string(move.square_);
+    line += std::to_string(move.vertical_);
+    line += std::to_string(move.horizontal_);
+  }
+  return line;
 }
 
 std::string GameApplication::getWinCoords(GAME_INDEX index) const
 {
-    std::string line;
-    auto coords = manager_.getGame(index).getWinCoords();
-    for (auto coord : coords)
-    {
-        line += std::to_string(coord.square_);
-        line += std::to_string(coord.vertical_);
-        line += std::to_string(coord.horizontal_);
-    }
-    return line;
+  std::string line;
+  auto coords = manager_.getGame(index).getWinCoords();
+  for (auto coord : coords)
+  {
+    line += std::to_string(coord.square_);
+    line += std::to_string(coord.vertical_);
+    line += std::to_string(coord.horizontal_);
+  }
+  return line;
 }
 
 bool GameApplication::exist(GAME_INDEX index) const
 {
-    try {
-          manager_.getGame(index);
-    }  catch (std::out_of_range& ) {
-        return false;
-    }
-    return true;
+  try
+  {
+    manager_.getGame(index);
+  }
+  catch (std::out_of_range&)
+  {
+    return false;
+  }
+  return true;
 }
 
 Player GameApplication::getPlayerToMove(GAME_INDEX index) const
 {
-    return manager_.getGame(index).getPlayerToMove();
+  return manager_.getGame(index).getPlayerToMove();
 }
 
 std::string GameApplication::getBoard(GAME_INDEX index) const
 {
-    auto arrayBoard = manager_.getGame(index).getBoard();
-    std::string result;
-    std::copy(std::cbegin(arrayBoard), std::cend(arrayBoard), std::back_inserter(result));
-    return result;
+  auto arrayBoard = manager_.getGame(index).getBoard();
+  std::string result;
+  std::copy(std::cbegin(arrayBoard), std::cend(arrayBoard),
+            std::back_inserter(result));
+  return result;
 }
--- src/gameapplication.h
+++ src/gameapplication.h
@@ -6,28 +6,29 @@
 
 class GameApplication
 {
-public:
-    GameApplication();
-    void init();
-    void finalize();
-    void debugPrint(GAME_INDEX index);      // Only for debug
+ public:
+  GameApplication();
+  void init();
+  void finalize();
+  void debugPrint(GAME_INDEX index);  // Only for debug
 
-    // commands
-    GAME_INDEX createNewGame();
-    void setNewMove(GAME_INDEX index, Player player, const Coordinates& coords);
-    void releaseGame(GAME_INDEX index);
+  // commands
+  GAME_INDEX createNewGame();
+  void setNewMove(GAME_INDEX index, Player player, const Coordinates& coords);
+  void releaseGame(GAME_INDEX index);
 
-    // queries
-    bool finished(GAME_INDEX index) const;
-    bool started(GAME_INDEX index) const;
-    uint8_t result(GAME_INDEX index) const;
-    std::string getMoves(GAME_INDEX index) const;
-    std::string getWinCoords(GAME_INDEX index) const;
-    bool exist(GAME_INDEX index) const;
-    Player getPlayerToMove(GAME_INDEX index) const;
-    std::string getBoard(GAME_INDEX index) const;
-private:
-    GameManager manager_;   // now only one manager
+  // queries
+  bool finished(GAME_INDEX index) const;
+  bool started(GAME_INDEX index) const;
+  uint8_t result(GAME_INDEX index) const;
+  std::string getMoves(GAME_INDEX index) const;
+  std::string getWinCoords(GAME_INDEX index) const;
+  bool exist(GAME_INDEX index) const;
+  Player getPlayerToMove(GAME_INDEX index) const;
+  std::string getBoard(GAME_INDEX index) const;
+
+ private:
+  GameManager manager_;  // now only one manager
 };
 
-#endif // GAMEAPPLICATION_H
+#endif  // GAMEAPPLICATION_H
