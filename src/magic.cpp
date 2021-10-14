ea27464cbcf2aafe982cc659f56daf6486edd5af
Denis Shihalev <shihalev.denis@gmail.com>
2021-10-14 21:42:54 +0300

    Restyle magic

--- src/magic.cpp
+++ src/magic.cpp
@@ -1,118 +1,130 @@
 #include "magic.h"
-#include "common_types.h"
-#include "bitboardhelpers.h"
 
-#include <set>
 #include <algorithm>
+#include <set>
+
+#include "bitboardhelpers.h"
+#include "common_types.h"
 
 namespace magic
 {
-
 std::array<BITBOARD, 76> winPositions;
 
 enum IndexRange
 {
-    UP,
-    _1,
-    _2,
-    _3,
-    _4,
-    DOWN
+  UP,
+  _1,
+  _2,
+  _3,
+  _4,
+  DOWN
 };
 
 bool isValidIndexes(IndexRange i1, IndexRange i2, IndexRange i3)
 {
-    if ((i1 > IndexRange::UP && i1 < IndexRange::DOWN) &&
-        (i2 > IndexRange::UP && i2 < IndexRange::DOWN) &&
-        (i3 > IndexRange::UP && i3 < IndexRange::DOWN))
-        return false;
-    else
-        return true;
+  if ((i1 > IndexRange::UP && i1 < IndexRange::DOWN) &&
+      (i2 > IndexRange::UP && i2 < IndexRange::DOWN) &&
+      (i3 > IndexRange::UP && i3 < IndexRange::DOWN))
+    return false;
+  else
+    return true;
 }
 
 BITBOARD getBitboardFromRanges(IndexRange i1, IndexRange i2, IndexRange i3)
 {
-    BITBOARD bb = 0;
-    std::array<Coordinates, 4> winLine;
+  BITBOARD bb = 0;
+  std::array<Coordinates, 4> winLine;
 
-    // first index
-    for (std::size_t i =0 ; i<4; ++i)
+  // first index
+  for (std::size_t i = 0; i < 4; ++i)
+  {
+    switch (i1)
     {
-        switch (i1)
-        {
-        case _1: case _2: case _3: case _4:
-            winLine[i].square_ = static_cast<COORD_INDEX>(i1)-1;
-            break;
-        case UP:
-            winLine[i].square_ = static_cast<COORD_INDEX>(i);
-            break;
-        case DOWN:
-            winLine[i].square_ = static_cast<COORD_INDEX>(3 - i);
-            break;
-        }
+      case _1:
+      case _2:
+      case _3:
+      case _4:
+        winLine[i].square_ = static_cast<COORD_INDEX>(i1) - 1;
+        break;
+      case UP:
+        winLine[i].square_ = static_cast<COORD_INDEX>(i);
+        break;
+      case DOWN:
+        winLine[i].square_ = static_cast<COORD_INDEX>(3 - i);
+        break;
     }
+  }
 
-    //second index
-    for (std::size_t i =0 ; i<4; ++i)
+  // second index
+  for (std::size_t i = 0; i < 4; ++i)
+  {
+    switch (i2)
     {
-        switch (i2)
-        {
-        case UP:
-            winLine[i].vertical_ = static_cast<COORD_INDEX>(i);
-            break;
-        case _1: case _2: case _3: case _4:
-            winLine[i].vertical_ = static_cast<COORD_INDEX>(i2)-1;
-            break;
-        case DOWN:
-            winLine[i].vertical_ = static_cast<COORD_INDEX>(3 - i);
-            break;
-        }
+      case UP:
+        winLine[i].vertical_ = static_cast<COORD_INDEX>(i);
+        break;
+      case _1:
+      case _2:
+      case _3:
+      case _4:
+        winLine[i].vertical_ = static_cast<COORD_INDEX>(i2) - 1;
+        break;
+      case DOWN:
+        winLine[i].vertical_ = static_cast<COORD_INDEX>(3 - i);
+        break;
     }
+  }
 
-
-    //third index
-    for (std::size_t i =0 ; i<4; ++i)
+  // third index
+  for (std::size_t i = 0; i < 4; ++i)
+  {
+    switch (i3)
     {
-        switch (i3)
-        {
-        case UP:
-            winLine[i].horizontal_ = static_cast<COORD_INDEX>(i);
-            break;
-        case _1: case _2: case _3: case _4:
-            winLine[i].horizontal_ = static_cast<COORD_INDEX>(i3)-1;
-            break;
-        case DOWN:
-            winLine[i].horizontal_ = static_cast<COORD_INDEX>(3 - i);
-            break;
-        }
+      case UP:
+        winLine[i].horizontal_ = static_cast<COORD_INDEX>(i);
+        break;
+      case _1:
+      case _2:
+      case _3:
+      case _4:
+        winLine[i].horizontal_ = static_cast<COORD_INDEX>(i3) - 1;
+        break;
+      case DOWN:
+        winLine[i].horizontal_ = static_cast<COORD_INDEX>(3 - i);
+        break;
     }
+  }
 
-    for (std::size_t i =0 ; i<4; ++i)
-    {
-        bb |= getBoardFromCoordinates(winLine[i]);
-    }
+  for (std::size_t i = 0; i < 4; ++i)
+  {
+    bb |= getBoardFromCoordinates(winLine[i]);
+  }
 
-    return bb;
+  return bb;
 }
 
 void generateWinPositions()
 {
-    std::set<BITBOARD> bbSet;
+  std::set<BITBOARD> bbSet;
 
-    for (int i = IndexRange::UP; i<=IndexRange::DOWN; ++i)
+  for (int i = IndexRange::UP; i <= IndexRange::DOWN; ++i)
+  {
+    for (int j = IndexRange::UP; j <= IndexRange::DOWN; ++j)
     {
-        for (int j=IndexRange::UP; j<=IndexRange::DOWN; ++j)
+      for (int k = IndexRange::UP; k <= IndexRange::DOWN; ++k)
+      {
+        if (isValidIndexes(static_cast<IndexRange>(i),
+                           static_cast<IndexRange>(j),
+                           static_cast<IndexRange>(k)))
         {
-            for (int k = IndexRange::UP; k<=IndexRange::DOWN; ++k)
-            {
-                if (isValidIndexes(static_cast<IndexRange>(i), static_cast<IndexRange>(j), static_cast<IndexRange>(k)))
-                {
-                    bbSet.insert(getBitboardFromRanges(static_cast<IndexRange>(i), static_cast<IndexRange>(j), static_cast<IndexRange>(k)));
-                }
-            }
+          bbSet.insert(getBitboardFromRanges(static_cast<IndexRange>(i),
+                                             static_cast<IndexRange>(j),
+                                             static_cast<IndexRange>(k)));
         }
+      }
     }
-    std::copy(std::begin(bbSet), std::end(bbSet), std::begin(winPositions));
+  }
+  std::copy(std::begin(bbSet), std::end(bbSet), std::begin(winPositions));
 }
 
-}
+}  // namespace magic
--- src/magic.h
+++ src/magic.h
@@ -2,13 +2,14 @@
 #define MAGIC_H
 
 #include <array>
+
 #include "board_types.h"
 
 namespace magic
 {
-    extern std::array<BITBOARD, 76> winPositions;
+extern std::array<BITBOARD, 76> winPositions;
 
-    void generateWinPositions();
+void generateWinPositions();
 
-}
-#endif // MAGIC_H
+}  // namespace magic
+#endif  // MAGIC_H
