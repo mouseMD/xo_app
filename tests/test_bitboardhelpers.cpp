15c70e70fcd0965887dced252b338543875904a0
Denis Shihalev <shihalev.denis@gmail.com>
2021-10-14 21:57:54 +0300

    Restyle tests

--- tests/test_bitboardhelpers.cpp
+++ tests/test_bitboardhelpers.cpp
@@ -1,109 +1,106 @@
-#include "catch.hpp"
-
+#include <bitset>
 #include <iostream>
 
-#include <bitset>
 #include "bitboardhelpers.h"
+#include "catch.hpp"
 
 using namespace std;
 
 TEST_CASE("Test getBoardFromCoordinates", "[bitboardhelpers]")
 {
-    SECTION("0 0 0 -> 1")
-    {
-        Coordinates coords(0, 0, 0);
-        BITBOARD bb = getBoardFromCoordinates(coords);
-        REQUIRE(bb == 1);
-    }
-    SECTION("0 0 3 -> 2^3")
-    {
-        Coordinates coords(0, 0, 3);
-        BITBOARD bb = getBoardFromCoordinates(coords);
-        REQUIRE(bb == 8);
-    }
-    SECTION("1 0 0 -> 2^16")
-    {
-        Coordinates coords(1, 0, 0);
-        BITBOARD bb = getBoardFromCoordinates(coords);
-        REQUIRE(bb == (1ull << 16));
-    }
+  SECTION("0 0 0 -> 1")
+  {
+    Coordinates coords(0, 0, 0);
+    BITBOARD bb = getBoardFromCoordinates(coords);
+    REQUIRE(bb == 1);
+  }
+  SECTION("0 0 3 -> 2^3")
+  {
+    Coordinates coords(0, 0, 3);
+    BITBOARD bb = getBoardFromCoordinates(coords);
+    REQUIRE(bb == 8);
+  }
+  SECTION("1 0 0 -> 2^16")
+  {
+    Coordinates coords(1, 0, 0);
+    BITBOARD bb = getBoardFromCoordinates(coords);
+    REQUIRE(bb == (1ull << 16));
+  }
 }
 
 TEST_CASE("Test getCoordinatesFromBoard", "[bitboardhelpers]")
 {
-    BITBOARD bb = 2;
-    Coordinates coord = getCoordinatesFromBoard(bb);
-    REQUIRE(coord.square_ == 0);
-    REQUIRE(coord.vertical_ == 0);
-    REQUIRE(coord.horizontal_ == 1);
+  BITBOARD bb = 2;
+  Coordinates coord = getCoordinatesFromBoard(bb);
+  REQUIRE(coord.square_ == 0);
+  REQUIRE(coord.vertical_ == 0);
+  REQUIRE(coord.horizontal_ == 1);
 }
 
-
-TEST_CASE( "From coords to coords", "[bitboardhelpers]")
+TEST_CASE("From coords to coords", "[bitboardhelpers]")
 {
-    for (uint8_t i = 0; i<4; ++i)
+  for (uint8_t i = 0; i < 4; ++i)
+  {
+    for (uint8_t j = 0; j < 4; ++j)
     {
-        for (uint8_t j = 0; j<4; ++j)
-        {
-            for (uint8_t k=0; k<4; ++k)
-            {
-                Coordinates coords(i, j, k);
-                BITBOARD bb = getBoardFromCoordinates(coords);
-                Coordinates coords2 = getCoordinatesFromBoard(bb);
-                REQUIRE(coords.square_ == coords2.square_);
-                REQUIRE(coords.vertical_ == coords2.vertical_);
-                REQUIRE(coords.horizontal_ == coords2.horizontal_);
-            }
-        }
+      for (uint8_t k = 0; k < 4; ++k)
+      {
+        Coordinates coords(i, j, k);
+        BITBOARD bb = getBoardFromCoordinates(coords);
+        Coordinates coords2 = getCoordinatesFromBoard(bb);
+        REQUIRE(coords.square_ == coords2.square_);
+        REQUIRE(coords.vertical_ == coords2.vertical_);
+        REQUIRE(coords.horizontal_ == coords2.horizontal_);
+      }
     }
+  }
 }
 
 TEST_CASE("Test getWinCoordsFromBoard", "[bitboardhelpers]")
 {
-    SECTION("000 -- 333")
-    {
-        BITBOARD bb1 = getBoardFromCoordinates(Coordinates(0, 0, 0)),
-                bb2 = getBoardFromCoordinates(Coordinates(1, 1, 1)),
-                bb3 = getBoardFromCoordinates(Coordinates(2, 2, 2)),
-                bb4 = getBoardFromCoordinates(Coordinates(3, 3, 3));
+  SECTION("000 -- 333")
+  {
+    BITBOARD bb1 = getBoardFromCoordinates(Coordinates(0, 0, 0)),
+             bb2 = getBoardFromCoordinates(Coordinates(1, 1, 1)),
+             bb3 = getBoardFromCoordinates(Coordinates(2, 2, 2)),
+             bb4 = getBoardFromCoordinates(Coordinates(3, 3, 3));
 
-        BITBOARD win = bb1 | bb2 | bb3 | bb4;
-        auto winCoords = getWinCoordsFromBoard(win);
-        REQUIRE(winCoords[0].square_ == 0);
-        REQUIRE(winCoords[0].vertical_ == 0);
-        REQUIRE(winCoords[0].horizontal_ == 0);
-        REQUIRE(winCoords[1].square_ == 1);
-        REQUIRE(winCoords[1].vertical_ == 1);
-        REQUIRE(winCoords[1].horizontal_ == 1);
-        REQUIRE(winCoords[2].square_ == 2);
-        REQUIRE(winCoords[2].vertical_ == 2);
-        REQUIRE(winCoords[2].horizontal_ == 2);
-        REQUIRE(winCoords[3].square_ == 3);
-        REQUIRE(winCoords[3].vertical_ == 3);
-        REQUIRE(winCoords[3].horizontal_ == 3);
-    }
+    BITBOARD win = bb1 | bb2 | bb3 | bb4;
+    auto winCoords = getWinCoordsFromBoard(win);
+    REQUIRE(winCoords[0].square_ == 0);
+    REQUIRE(winCoords[0].vertical_ == 0);
+    REQUIRE(winCoords[0].horizontal_ == 0);
+    REQUIRE(winCoords[1].square_ == 1);
+    REQUIRE(winCoords[1].vertical_ == 1);
+    REQUIRE(winCoords[1].horizontal_ == 1);
+    REQUIRE(winCoords[2].square_ == 2);
+    REQUIRE(winCoords[2].vertical_ == 2);
+    REQUIRE(winCoords[2].horizontal_ == 2);
+    REQUIRE(winCoords[3].square_ == 3);
+    REQUIRE(winCoords[3].vertical_ == 3);
+    REQUIRE(winCoords[3].horizontal_ == 3);
+  }
 
-    SECTION("021 -- 321")
-    {
-        BITBOARD bb1 = getBoardFromCoordinates(Coordinates(0, 2, 1)),
-                bb2 = getBoardFromCoordinates(Coordinates(1, 2, 1)),
-                bb3 = getBoardFromCoordinates(Coordinates(2, 2, 1)),
-                bb4 = getBoardFromCoordinates(Coordinates(3, 2, 1));
+  SECTION("021 -- 321")
+  {
+    BITBOARD bb1 = getBoardFromCoordinates(Coordinates(0, 2, 1)),
+             bb2 = getBoardFromCoordinates(Coordinates(1, 2, 1)),
+             bb3 = getBoardFromCoordinates(Coordinates(2, 2, 1)),
+             bb4 = getBoardFromCoordinates(Coordinates(3, 2, 1));
 
-        BITBOARD win = bb1 | bb2 | bb3 | bb4;
-        auto winCoords = getWinCoordsFromBoard(win);
-        REQUIRE(winCoords[0].square_ == 0);
-        REQUIRE(winCoords[0].vertical_ == 2);
-        REQUIRE(winCoords[0].horizontal_ == 1);
-        REQUIRE(winCoords[1].square_ == 1);
-        REQUIRE(winCoords[1].vertical_ == 2);
-        REQUIRE(winCoords[1].horizontal_ == 1);
-        REQUIRE(winCoords[2].square_ == 2);
-        REQUIRE(winCoords[2].vertical_ == 2);
-        REQUIRE(winCoords[2].horizontal_ == 1);
-        REQUIRE(winCoords[3].square_ == 3);
-        REQUIRE(winCoords[3].vertical_ == 2);
-        REQUIRE(winCoords[3].horizontal_ == 1);
-    }
+    BITBOARD win = bb1 | bb2 | bb3 | bb4;
+    auto winCoords = getWinCoordsFromBoard(win);
+    REQUIRE(winCoords[0].square_ == 0);
+    REQUIRE(winCoords[0].vertical_ == 2);
+    REQUIRE(winCoords[0].horizontal_ == 1);
+    REQUIRE(winCoords[1].square_ == 1);
+    REQUIRE(winCoords[1].vertical_ == 2);
+    REQUIRE(winCoords[1].horizontal_ == 1);
+    REQUIRE(winCoords[2].square_ == 2);
+    REQUIRE(winCoords[2].vertical_ == 2);
+    REQUIRE(winCoords[2].horizontal_ == 1);
+    REQUIRE(winCoords[3].square_ == 3);
+    REQUIRE(winCoords[3].vertical_ == 2);
+    REQUIRE(winCoords[3].horizontal_ == 1);
+  }
 }
-
--- tests/test_magic.cpp
+++ tests/test_magic.cpp
@@ -1,28 +1,27 @@
-#include "catch.hpp"
-
 #include <iostream>
 
-#include "magic.h"
-#include "board_4_4_4.h"
 #include "bitboardhelpers.h"
+#include "board_4_4_4.h"
+#include "catch.hpp"
+#include "magic.h"
 
 TEST_CASE("Test getBitboardFromRanges", "[magic]")
 {
-    Board_4_4_4 board;
+  Board_4_4_4 board;
 
-    magic::generateWinPositions();
+  magic::generateWinPositions();
 
-    for (int i =0 ; i< 76; i++)
+  for (int i = 0; i < 76; i++)
+  {
+    auto winLine = getWinCoordsFromBoard(magic::winPositions[i]);
+
+    for (auto coord : winLine)
     {
-        auto winLine = getWinCoordsFromBoard(magic::winPositions[i]);
-
-        for (auto coord : winLine)
-        {
-            board.setMark(X_Player, coord);
-        }
-        board.debugPrint();
-        board.clear();
-
-        REQUIRE(1 == 1);
+      board.setMark(X_Player, coord);
     }
+    board.debugPrint();
+    board.clear();
+
+    REQUIRE(1 == 1);
+  }
 }
