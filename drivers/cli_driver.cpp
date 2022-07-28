f07b96e36686c242e1fe29fc095e317f5f985802
Denis Shihalev <shihalev.denis@gmail.com>
2022-07-28 21:58:33 +0300

    Refactor cli_driver.cpp

--- drivers/cli_driver.cpp
+++ drivers/cli_driver.cpp
@@ -1,35 +1,54 @@
 #include "gameapplication.h"
 #include <iostream>
 
-int main()
+#include "game3d_4_4_4.h"
+#include "magic.h"
+
+Player reversePlayer(Player player)
 {
-    GameApplication ga;
-    auto index = ga.createNewGame();
-    bool notFinished = true;
+    return (player == Player::X_Player) ? Player::O_Player : Player::X_Player;
+}
+
+template <typename T>
+std::string convertCoordsToString(T coordsList)
+{
+    std::string line;
+    for (auto coord : coordsList)
+    {
+      line += std::to_string(coord.square_);
+      line += std::to_string(coord.vertical_);
+      line += std::to_string(coord.horizontal_);
+    }
+    return line;
+}
+
+void playTestGame()
+{
+    Game3D_4_4_4 game(1);   // TODO make game default-constructible
+    magic::generateWinPositions();  // TODO move this initialization to proper place
     auto player = Player::X_Player;
-    ga.debugPrint(index);
-    while (notFinished)
+    game.debugPrint();
+    while (!game.isFinished())
     {
         int i1, i2, i3;
         std::cout << "It's your move:\n";
         std::cin >> i1 >> i2 >> i3;
-        ga.setNewMove(index, player, {static_cast<COORD_INDEX>(i1),
-                                                static_cast<COORD_INDEX>(i2),
-                                                static_cast<COORD_INDEX>(i3)});
-        ga.debugPrint(index);
-        player = (player == Player::X_Player) ? Player::O_Player : Player::X_Player;
-        std::cout  << "Exists: " << ga.exist(index) << "\n";
-        std::cout  << "Started: " << ga.started(index) << "\n";
-        std::cout  << "Finished: " << ga.finished(index) << "\n";
-        std::cout  << "Result: " << ga.result(index) << "\n";
-        std::cout  << "Moves: " << ga.getMoves(index) << "\n";
-        std::cout  << "Win coords: " << ga.getWinCoords(index) << "\n";
-
-        if (ga.finished(index))
-        {
-            notFinished = false;
-            ga.releaseGame(index);
-        }
+        game.setNewMove(player, {static_cast<COORD_INDEX>(i1),
+                                 static_cast<COORD_INDEX>(i2),
+                                 static_cast<COORD_INDEX>(i3)});
+        game.debugPrint();
+        player = reversePlayer(player);
+        std::cout  << "Started: " << game.isInProcess() << "\n";
+        std::cout  << "Finished: " << game.isFinished() << "\n";
+        std::cout  << "Result: " << game.result() << "\n";
+        std::cout  << "Moves: " << convertCoordsToString(game.getMoves()) << "\n";
+        std::cout  << "Win coords: " << convertCoordsToString(game.getWinCoords()) << "\n";
     }
+    std::cout << "The game is finished!" << std::endl;
+}
+
+int main()
+{
+    playTestGame();
 }
 
